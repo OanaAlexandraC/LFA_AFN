709e6199b5bf631e20234205059bbc4e61e6a8e8
OanaAlexandraC <36674400+OanaAlexandraC@users.noreply.github.com>
2018-02-25 04:41:11 +0200

    Add constructor to class

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -14,9 +15,36 @@ class AFN {
     } *T; //tranzitii (din starea s1 in starea s2 prin simbolul c)
     int nf; //numarul de stari finale ale automatului
     char *F; //multimea starilor finale
+public:
+    explicit AFN(const char *fisier); //constructor AFN
 };
 
+AFN::AFN(const char *fisier) {
+    n = s = nrt = nf = 0;
+    stare = 0;
+    ifstream f(fisier); //deschidere fisier
+    int i;
+    f >> n; //citire numar de stari ale automatului
+    Q = new char[n];
+    for (i = 0; i < n; i++)
+        f >> Q[i]; //citire multime de stari ale automatului
+    f >> s; //citire numar de simboluri ale alfabetului de intrare
+    A = new char[s];
+    for (i = 0; i < s; i++)
+        f >> A[i]; //citire alfabet de intrare
+    f >> stare; //citire stare initiala
+    f >> nrt; //citire numar de tranzitii
+    T = new delta[nrt];
+    for (i = 0; i < nrt; i++)
+        f >> T[i].s1 >> T[i].c >> T[i].s2; //citire tranzitii
+    f >> nf; //citire numar de stari finale ale automatului
+    F = new char[nf];
+    for (i = 0; i < nf; i++)
+        f >> F[i]; //citire multime de stari finale ale automatului
+    f.close();
+}
+
 int main() {
-    cout << "Hello, World!";
+    AFN x("date.txt"); //declarare automat
     return 0;
 }
