fa1cc18d8f842ada32797c1f1fec2dc2c49260f0
OanaAlexandraC <36674400+OanaAlexandraC@users.noreply.github.com>
2018-02-25 04:10:57 +0200

    add class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
+using namespace std;
+
+class AFN {
+    int n; //numarul de stari ale automatului
+    char *Q; //multimea de stari ale automatului
+    int s; //numarul simbolurilor din alfabetul de intrare
+    char *A; //alfabetul de intrare
+    char stare; //starea initiala a alfabetului
+    int nrt; //numar de tranzitii
+    struct delta {
+        char s1, s2, c;
+    } *T; //tranzitii (din starea s1 in starea s2 prin simbolul c)
+    int nf; //numarul de stari finale ale automatului
+    char *F; //multimea starilor finale
+};
+
 int main() {
-    std::cout << "Hello, World" << std::endl;
+    cout << "Hello, World!";
     return 0;
 }
